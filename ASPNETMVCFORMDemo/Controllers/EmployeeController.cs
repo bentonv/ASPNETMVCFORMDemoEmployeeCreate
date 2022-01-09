3c5fc4d75bfed7c8f86300efec6a9128eeeb38de
bjval <bjval@DESKTOP-T8601U9>
2022-01-09 13:19:01 -0800

    Add project files.

---
+++ ASPNETMVCFORMDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPNETMVCFORMDemo.Models;
+/*Benton Valenzuela lab2
+ I tried to extend the idea by adding a multi select. 
+i rant into an issue with validation. I think I could combine the code from the two HTTP posts into a single block
+*/
+namespace ASPNETMVCFORMDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var model = new EmployeeInfo
+            {
+
+                SelectedCities = new[] { 2 },
+                CityId = GetCities()
+            };
+            return View(model);
+        }
+
+       
+        private SelectListItem[] GetDepartments()
+        {
+            var list = new List<SelectListItem>();
+
+            list.Add(new SelectListItem()
+            {
+                Text = "Accounting", 
+                Value = "101",
+                Selected = true
+            });
+
+            list.Add(new SelectListItem()
+            {
+                Text = "Human Resources",
+                Value = "105",
+               
+            });
+
+            list.Add(new SelectListItem()
+            {
+                Text = "Information Technology",
+                Value = "110",
+                
+            });
+            list.Add(new SelectListItem()
+            {
+                Text = "Executive",
+                Value = "115",
+               
+            });
+            return list.ToArray();
+        }
+
+        
+        private List<SelectListItem> GetCities()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem() { Text = "Napa", Value = "1" });
+            items.Add(new SelectListItem() { Text = "Calistoga", Value = "2" });
+            items.Add(new SelectListItem() { Text = "St Helena", Value = "3" });
+            items.Add(new SelectListItem() { Text = "American Canyon", Value = "4" });
+            return items;
+        }
+        public ActionResult Create()
+        {
+            ViewData["Departments"] = GetDepartments();
+            ViewData["Cities"] = GetCities();
+            //call get departments method then assign it to viewdata dictionary
+            //create new object model and pass it to view method
+            return View(new Models.EmployeeInfo());
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeInfo e)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    SaveEmployeeToDatabase(e);
+                    return RedirectToAction("Index");
+                }
+                catch(Exception ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                }
+            }
+            ViewData["Departments"] = GetDepartments();
+            ViewData["Cities"] = GetCities();
+            return View(e);
+        }
+
+        //[HttpPost]
+        //public ActionResult Index(EmployeeInfo model)
+        //{
+        //    model.CityId = GetCities();
+        //    if (model.SelectedCities != null)
+        //    {
+        //        List<SelectListItem> selectedItems = model.CityId.Where(p => model.SelectedCities.Contains(int.Parse(p.Value))).ToList();
+        //        foreach (var City in selectedItems)
+        //        {
+        //            City.Selected = true;
+        //            ViewBag.Message += City.Text + " | ";
+        //        }
+        //    }
+        //    return View(model);
+        //}
+        private void SaveEmployeeToDatabase(EmployeeInfo e)
+        {
+            throw new ApplicationException("Could not save employee to database");
+        }
+    }
+}
---
+++ ASPNETMVCFORMDemo/Models/EmployeeInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+
+namespace ASPNETMVCFORMDemo.Models
+{
+    public enum Gender
+    {
+        Female, Male
+    }
+    public class EmployeeInfo
+    {
+        
+        public int EmployeeID { get; set; }
+
+        [Display(Name = "Given Name")]
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+        
+        [Display(Name = "SurName")]
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+        public Gender Gender { get; set; }
+
+        [Display(Name = "Active?")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Annual Salary")]
+        [Range(1, 2000000)]
+        public double  AnnualSalary { get; set; }
+
+        [Display(Name = "Department")]
+        [Range(1, 1000)]
+        public int  DepartmentId { get; set; }
+
+        [Display(Name = "City")]
+        
+        //public int CityId { get; set; }
+        
+        public IEnumerable<SelectListItem> CityId { get; set; }
+
+        public int[] SelectedCities { get; set; }
+
+    }
+}
